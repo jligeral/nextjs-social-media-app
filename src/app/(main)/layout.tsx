891568ae0754f43b815ce28a4700a23ef2b7f8a5
James Ligeralde <jligeral@hawaii.edu>
2024-08-20 19:01:23 -1000

    Created MenuBar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import {validateRequest} from '@/auth';
 import {redirect} from 'next/navigation';
 import SessionProvider from './SessionProvider';
 import Navbar from './Navbar';
+import MenuBar from "@/app/(main)/MenuBar";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const session = await validateRequest();
@@ -11,9 +12,11 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return <SessionProvider value={session}>
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <div className="mx-auto max-w-7xl p-5">
+      <div className="mx-auto max-w-7xl p-5 flex w-full grow gap-5">
+        <MenuBar className="sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80" />
         {children}
       </div>
+      <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
     </div>
   </SessionProvider>
 }
