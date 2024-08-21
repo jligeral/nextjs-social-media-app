f83958666d04998d971ac2d1284b080598ee359d
James Ligeralde <jligeral@hawaii.edu>
2024-08-21 13:45:04 -1000

    Minor site changes
    bugbook -> gacha

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl overflow-hidden bg-card shadow-2xl">
         <div className="md:w-1/2 w-full space-y-10 overflow-y-auto p-10">
-          <h1 className="text-center text-3xl font-bold">Login to bugbook</h1>
+          <h1 className="text-center text-3xl font-bold">Login to gotcha</h1>
           <div className="space-y-5">
             <LoginForm/>
             <Link href="/signup" className="block text-center hover:underline">
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -14,9 +14,9 @@ export default function Page() {
      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl overflow-hidden bg-card shadow-2xl">
        <div className="md:w-1/2 w-full space-y-10 overflow-y-auto p-10">
          <div className="space-y-1 text-center">
-           <h1 className="text-3xl font-bold">Sign up to bugbook</h1>
+           <h1 className="text-3xl font-bold">Sign up to gotcha</h1>
            <p className="text-muted-foreground">
-             A place where even <span className="italic">you</span> can find a friend
+             A place to share gacha experiences
            </p>
          </div>
          <div className="space-y-5">
