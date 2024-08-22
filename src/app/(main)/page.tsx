f4cbd0821d7a98eab81e1e5790e523e250454fb3
James Ligeralde <jligeral@hawaii.edu>
2024-08-22 09:14:53 -1000

    Created trends sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
 import { postDataInclude } from "@/lib/types";
+import TrendsSideBar from "@/components/TrendsSideBar";
 
 export default async function Home()  {
 
@@ -13,13 +14,14 @@ export default async function Home()  {
   });
 
   return (
-    <main className="w-full min-w-0">
+    <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
         <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSideBar />
     </main>
   );
 }
