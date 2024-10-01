9673208365911dd4f20cf9901e810228694d888c
James Ligeralde <jligeral@hawaii.edu>
2024-10-01 13:50:31 -1000

    Google Signin via OAuth implemented

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@ import loginImage from "@/assets/login-image.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import LoginForm from "@/app/(auth)/login/LoginForm";
+import GoogleSignInButton from "@/app/(auth)/login/google/GoogleSignInButton";
 
 export const metadata: Metadata = {
   title: "Login"
@@ -15,6 +16,12 @@ export default function Page() {
         <div className="md:w-1/2 w-full space-y-10 overflow-y-auto p-10">
           <h1 className="text-center text-3xl font-bold">Login to gotcha</h1>
           <div className="space-y-5">
+            <GoogleSignInButton/>
+            <div className={`flex items-center gap-3`}>
+              <div className={`h-px flex-1 bg-muted`} />
+              <span>OR</span>
+              <div className={`h-px flex-1 bg-muted`} />
+            </div>
             <LoginForm/>
             <Link href="/signup" className="block text-center hover:underline">
               Don&apos;t have an account? Sign Up
